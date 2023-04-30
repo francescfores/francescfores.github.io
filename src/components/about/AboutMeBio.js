e568d3baff12fe13aa875c3c08c60b1a5724366c
francesc fores <ffores93@gmail.com>
2023-04-30 23:19:08 +0200

    update

--- src/components/about/AboutMeBio.js
+++ src/components/about/AboutMeBio.js
@@ -1,4 +1,4 @@
-import profileImage from '../../images/profile.jpeg';
+import profileImage from '../../images/profile.png';
 import { useContext } from 'react';
 import AboutMeContext from '../../context/AboutMeContext';
 
@@ -6,12 +6,16 @@ const AboutMeBio = () => {
 	const { aboutMe } = useContext(AboutMeContext);
 
 	return (
-		<div className="block sm:flex sm:gap-10 pt-20">
-			<div className="w-full sm:w-1/4 mb-7 sm:mb-0">
-				<img src={profileImage} className="rounded-lg w-96" alt="" />
+		<div className="grid sm:grid-cols-5 grid-cols-5 gap-10 pt-28 pb-8 ">
+			{/*<div className="mb-7 sm:mb-0">*/}
+			{/*	<img src={profileImage} className="rounded-lg" alt="" />*/}
+			{/*</div>*/}
+			<div className="col-span-5 sm:col-span-1 relative w-52 w-52 mx-auto ">
+				<img className="rounded-full border border-gray-100 shadow-sm"
+					 src={profileImage} alt="user image"/>
 			</div>
 
-			<div className="font-general-regular w-full sm:w-3/4 text-left">
+			<div className="col-span-5 sm:col-span-4 font-general-regular w-full sm:w-3/4 text-center sm:text-left mx-auto ">
 				{aboutMe.map((bio) => (
 					<p
 						className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
